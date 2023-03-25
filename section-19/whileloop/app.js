4a1961d7f93e3dc061aa3328c528a2b15ea2671c
elidvenega <venegaelid@gmail.com>
2023-03-25 11:16:49 -0700

    Worked with loops

--- section-19/whileloop/app.js
+++ section-19/whileloop/app.js
@@ -1,5 +1,25 @@
-let count = 0;
+// let count = 0;
 
-while(count < 10) {
-    console.log(count += 1);
+// while(count < 10) {
+//     console.log(count += 1);
+// }
+
+//////////////Password///////////////
+// const SECRET = "BabyHippo";
+
+// let guess = prompt("ENTER THE SECRET CODE");
+
+// while (guess !== SECRET) {
+//   guess = prompt("ENTER THE SECRET CODE");
+// }
+
+// console.log("CONGRATS YOU GOT THE SECRET");
+
+//////////////////////////////////////////////////////////////////////////
+
+let input = prompt("Hey, say something!");
+while (true) {
+  input = prompt(input);
+  if (input.toLowerCase() === "stop copying me") break;
 }
+console.log("OK YOU WIN!");
