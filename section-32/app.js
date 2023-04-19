f29b75757fb9ecaab6ca96563bae94d19f8338ee
elidvenega <venegaelid@gmail.com>
2023-04-18 20:09:42 -0700

    Updated

---
+++ section-32/app.js
@@ -0,0 +1,8 @@
+// const math = require('./math');
+const { PI, add, square } = require("./math");
+
+const cats = require("./shelter")
+// console.log(PI);
+// console.log(add(1, 7));
+
+console.log("Test",cats);
