fac48bc7e1f640ca88728e40d1f7f48543dbeae7
elidvenega <venegaelid@gmail.com>
2023-04-24 20:29:05 -0700

    Updated

---
+++ section-32/franc/index.js
@@ -0,0 +1,9 @@
+import { franc } from "franc";
+
+// console.log(franc("Alle menslike wesens word vry"));
+// console.log(franc('এটি একটি ভাষা একক IBM স্ক্রিপ্ট'));
+// console.log(franc('Alle menneske er fødde til fridom'));
+
+let word;
+
+console.log(franc(`${word}`));
--- section-33/my-code/index.js
+++ section-33/my-code/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
 
-app.use((req,res) => {
-    console.log("We got a new request");
-   res.send("Hello this is a response")
-})
+// app.use((req, res) => {
+//   console.log("We got a new request");
+//     res.send("Hello this is a response");
+//   res.send("<h1>This is the back end</h1>");
+// });
+
+app.get("/cats", (req, res) => {
+  res.send("Meow");
+});
+
+app.get("/dog", (req, res) => {
+  res.send("Woof");
+});
 
 app.listen(3000, () => {
-    console.log("Running");
+  console.log("Running");
 });
