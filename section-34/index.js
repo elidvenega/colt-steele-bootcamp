3ba36be69a0cbe7ec6e577eb1e4b8bd097d33796
elidvenega <venegaelid@gmail.com>
2023-04-26 19:42:19 -0700

    Worked on bootcamp

--- section-34/index.js
+++ section-34/index.js
@@ -9,9 +9,19 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
+app.get("/cats", (req, res) => {
+  const cats = ["Blue", "rocket", "Monty", "stephanie", "Winston"];
+  res.render("cats", { cats });
+});
+
+app.get("/r/:subreddit", (req, res) => {
+  const { subreddit } = req.params;
+  res.render("subreddit", { subreddit });
+});
+
 app.get("/rand", (req, res) => {
   const num = Math.floor(Math.random() * 10) + 1;
-  res.render("random", num);
+  res.render("random", { num: num });
 });
 
 app.listen(3000, () => {
