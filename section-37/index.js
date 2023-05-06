34a5ac8dde639f3bb1b893944b9b90bf688a538b
elidvenega <venegaelid@gmail.com>
2023-05-05 18:11:19 -0700

    Updated

--- section-37/index.js
+++ section-37/index.js
@@ -1,10 +1,20 @@
+// const mongoose = require("mongoose");
+// mongoose
+//   .connect(
+//     "mongoose.connect('mongodb://127.0.0.1:27017/movieApp', { useNewUrlParser: true, useUnifiedTopology: true }"
+//   )
+//   .then(() => {
+//     console.log("CONNECTION OPEN!!");
+//   })
+//   .catch((err) => {
+//     console.log("OH NO ERROE");
+//     console.log(err);
+//   });
+
 const mongoose = require("mongoose");
-mongoose.connect(
-  "mongoose.connect('mongodb://127.0.0.1:27017/movieApp', { useNewUrlParser: true, useUnifiedTopology: true }")
-   .then(() => {
-    console.log("CONNECTION OPEN!!");
-   })
-   .catch(err => {
-    console.log("OH NO ERROE");
-    console.log(err);
-   })
+mongoose.connect("mongodb://127.0.0.1:27017/test");
+
+const Cat = mongoose.model("Cat", { name: String });
+
+const kitty = new Cat({ name: "Zildjian" });
+kitty.save().then(() => console.log("meow"));
