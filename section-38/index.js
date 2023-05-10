f8bba3bc1da766151478f3283f56b64885c479d1
elidvenega <venegaelid@gmail.com>
2023-05-09 21:45:45 -0700

    Worked on bootcamp

--- section-38/index.js
+++ section-38/index.js
@@ -17,17 +17,28 @@ const Product = require("./models/product");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/products", async (req, res) => {
   const products = await Product.find({});
   res.render("products/index", { products });
 });
 
-// app.get("/products/:id", async (req, res) => {
-//   const { id } = req.params;
-//   const product = await Product.findById(id);
-//   res.render("products/show", { product });
-// });
+app.get("/products/new", (req, res) => {
+  res.render("products/new");
+});
+
+app.post("/products", async (req, res) => {
+  const newProduct = new Product(req.body);
+  await newProduct.save();
+  res.redirect(`/products/${newProduct._id}`);
+});
+
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/show", { product });
+});
 
 app.listen(3000, () => {
   console.log("APP IS LISTENING ON PORT 3000");
--- section-38/seeds.js
+++ section-38/seeds.js
@@ -27,35 +27,37 @@ mongoose
 
 const seedProducts = [
   {
-    name: "Ruby Grapefruit",
-    price: 1.99,
+    name: "Fairy Eggplant",
+    price: 1.0,
+    category: "vegetable",
+  },
+  {
+    name: "Organic Goddess Melon",
+    price: 4.99,
     category: "fruit",
   },
   {
-    name: "Apples",
+    name: "Organic Mini Seedless Watermelon",
     price: 3.99,
     category: "fruit",
   },
   {
-    name: "Socks",
-    price: 5.99,
-    category: "fruit",
+    name: "Organic Celery",
+    price: 1.5,
+    category: "vegetable",
   },
   {
-    name: "Juice",
-    price: 1.99,
-    category: "fruit",
+    name: "Chocolate Whole Milk",
+    price: 2.69,
+    category: "dairy",
   },
 ];
 
 Product.insertMany(seedProducts)
-.then(res => {
+  .then((res) => {
     console.log(res);
-})
-.catch(e => {
+  })
+  .catch((e) => {
     console.log(e);
     console.log("SOMETHING IS NOT WORKING");
-})
-
-
-
+  });
