21b5b43af486fe2ef2286035b7ff701bface6f0e
elidvenega <venegaelid@gmail.com>
2023-05-27 18:16:30 -0700

    Completed fetch project

--- complete-web-developer-bootcamp/section-28/mecodingalong/js/main.js
+++ complete-web-developer-bootcamp/section-28/mecodingalong/js/main.js
@@ -1,25 +1,26 @@
-const output = document.querySelector(".output");
-// const jokes = document.querySelector("#jokes");
-const button = document.querySelector("button");
-
-// const addNewJoke = async () => {
-//   const jokeText = await dadJokes();
-//   const newLI = document.createElement("LI");
-//   newLI.append(jokeText);
-//   jokes.append(newLI);
-// };
+const jokes = document.querySelector(".jokes");
+const button = document.querySelector(".button");
 
 async function dadJokes() {
-  const config = {
-    headers: {
-      Accept: "application/json",
-    }
-  };
-  const resp = await fetch("https://icanhazdadjoke.com", config);
-  const data = await resp.json();
-  console.log(data);
-  output.innerHTML = data.joke;
+  try {
+    const config = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+    const resp = await fetch("https://icanhazdadjoke.com", config);
+    const data = await resp.json();
+    return data.joke;
+  } catch (e) {
+    console.log("Error!", e);
+  }
 }
-dadJokes()
 
-button.addEventListener("click", dadJokes);
+async function newJoke() {
+  const jokeText = await dadJokes();
+  const newLI = document.createElement("LI");
+  newLI.append(jokeText);
+  jokes.append(newLI);
+}
+
+button.addEventListener("click", newJoke);
